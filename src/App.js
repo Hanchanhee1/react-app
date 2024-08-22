267b8661d7867705295f407ab246bb5ffb23dd70
chanhee han <mh981219@naver.com>
2024-08-22 10:17:59 +0900

    17 state 사용 , 18 key

--- src/App.js
+++ src/App.js
@@ -5,11 +5,22 @@ import Subject from "./components/Subject";
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      Subject:{title:'WEB', sub:'world wide web!'},
+      contents:[
+        {id:1, title:'HTML', desc:'HTML is HyperText ...'},
+        {id:2, title:'CSS', desc:'CSS is for design'},
+        {id:3, title:'JavaScript', desc:'JavaScript is for interactive'}
+      ]
+    }
+  }
   render() {
     return (
       <div className="App">
-        <Subject title="WEB" sub="world wide web!"></Subject>
-        <Nav></Nav>
+        <Subject title={this.state.Subject.title} sub={this.state.Subject.sub}></Subject>
+        <Nav data={this.state.contents}></Nav>
         <Content title="HTML" desc="HTML is HyperText Markup Language.."></Content>
       </div>
     );
