73b1a74dea0a30245ddfdab7629feea76aaa1385
chanhee han <mh981219@naver.com>
2024-08-30 12:51:09 +0900

    29 create 구현 : mode 변경 기능 , 30 create 구현 : mode 전환 기능

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import Nav from "./components/Nav";
-import Content from "./components/Content";
+import ReadContent from "./components/ReadContent";
+import CreateContent from "./components/CreateContent";
 import Subject from "./components/Subject";
+import Control from './components/Control';
 import './App.css';
 
 class App extends Component {
@@ -21,10 +23,11 @@ class App extends Component {
   }
   render() {
     console.log('App render')
-    var _title, _desc = null;
+    var _title, _desc, _article = null;
     if(this.state.mode === 'welcome') {
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
+      _article = <ReadContent title={_title} desc={_desc}></ReadContent>
     } else if(this.state.mode === 'read') {
       var i = 0;
       while(i < this.state.contents.length){
@@ -36,7 +39,9 @@ class App extends Component {
         }
         i = i + 1;
       }
-      
+      _article = <ReadContent title={_title} desc={_desc}></ReadContent>
+    }else if(this.state.mode === 'create') {
+      _article = <CreateContent></CreateContent>
     }
     console.log('render',this);
     return (
@@ -65,7 +70,12 @@ class App extends Component {
           });
         }.bind(this)}
          data={this.state.contents}></Nav>
-        <Content title={_title} desc={_desc}></Content>
+         <Control onChangeMode={function(_mode){
+          this.setState({
+            mode:_mode
+          });
+         }.bind(this)}></Control>
+        {_article}
       </div>
     );
   }
